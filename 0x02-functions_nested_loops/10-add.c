76dc7639fd605066792e20290856ad688db1308b
rnrnshn <olymangue@gmail.com>
2022-09-14 03:25:26 +0200

    add task 10 for functions and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @x: int to be added to x
+ * @y: int to be added to y
+ * Return: sum of x and y
+ */
+
+int add(int x, int y)
+{
+	int a;
+
+	a = x + y;
+	return (a);
+}
