ce6a67db5eab20c79b4fe724f41a1346d2d548b0
rnrnshn <olymangue@gmail.com>
2022-09-16 03:02:17 +0200

    add 2-mul.c file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN
 #define MAIN
-extern int _putchar(char c);
-extern int _isupper(int c);
-extern int _isdigit(int c);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 #endif
