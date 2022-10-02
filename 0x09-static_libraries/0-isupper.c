5e588c22119288de192616bd2445dd4c796b6ec9
rnrnshn <olymangue@gmail.com>
2022-10-02 05:39:11 +0200

    fix betty warnings

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,16 +1,13 @@
-	#include "main.h"
+#include "main.h"
+/**
+ * _isupper - check if is upper.
+ *
+ * @c: is an integer param
+ *
+ * Return: Always 0.
+ */
 
-	/**
-	* _isupper - check the code .
-	*
-	* @c: is an integer param
-	*
-	* Return: Always 0.
-	*/
-
-
-	int _isupper(int c)
-	{
-
-		return (c >= 65 && c <= 90);
-	}
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,20 +1,20 @@
-	#include "main.h"
+#include "main.h"
 
-	/**
-	* _memset
-	* @s: pointer to char params
-	* @b: data to change
-	* @n: index
-	* Return: *s
-	*/
+/**
+  * _memset - check the code.
+  * @s: pointer to char params
+  * @b: data to change
+  * @n: index
+  * Return: *s
+  */
 
-	char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
-		unsigned int i;
-
-		for (i = 0; i < n; i++)
-		{
-			s[i] = b;
-		}
-		return (s);
+		s[i] = b;
 	}
+	return (s);
+}
