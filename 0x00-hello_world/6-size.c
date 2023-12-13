aef11b7435fa4bd6173afe144f74120add2803e3
rnrnshn <olymangue@gmail.com>
2023-12-14 01:58:15 +0200

    fix 6-size.c typo on output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	printf("Size of a char is: %lu\n", (unsigned int long)sizeof(char));
+	printf("Size of a char: %lu\n", (unsigned int long)sizeof(char));
 	printf("Size of an int: %lu\n", (unsigned int long)sizeof(int));
 	printf("Size of a long int: %lu\n", (unsigned int long)sizeof(long int));
 	printf("Size of a long long int: %lu\n",
