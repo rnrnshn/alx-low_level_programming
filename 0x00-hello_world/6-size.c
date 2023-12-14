98e3fa867ebbe51dbb5e9fac328f50768b31ce92
rnrnshn <olymangue@gmail.com>
2023-12-14 02:02:33 +0200

    fix betty-style warning and fix typo on 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,12 @@
 
 int main(void)
 {
-	printf("Size of a char: %lu byte (s)\n", (unsigned int long)sizeof(char));
-	printf("Size of an int: %lu byte (s)\n", (unsigned int long)sizeof(int));
-	printf("Size of a long int: %lu byte (s)\n", (unsigned int long)sizeof(long int));
-	printf("Size of a long long int: %lu byte (s)\n",
+	printf("Size of a char: %lu byte(s)\n", (unsigned int long)sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned int long)sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n",
+	       (unsigned int long)sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n",
 	       (unsigned int long)sizeof(long long int));
-	printf("Size of a float: %lu byte (s)\n", (unsigned int long)sizeof(float));
+	printf("Size of a float: %lu byte(s)\n", (unsigned int long)sizeof(float));
 	return (0);
 }
